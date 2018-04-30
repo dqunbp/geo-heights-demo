91a81d81c4997dbec5e5bbb90e49c96a06f2b484
Tigran Grigoryan <dqunbp@gmail.com>
2018-05-01 01:00:24 +0300

    add demo data to the map

--- map.js
+++ map.js
@@ -1,15 +1,17 @@
 var map = new L.Map('map');
-map.setView([55.878358755645088, 37.705021995236301], 16, false);
+map.setView([55.87835875564509, 37.7050219952363], 16, false);
 
 new L.TileLayer('http://mt0.google.com/vt/lyrs=s&hl=en&x={x}&y={y}&z={z}&s=Ga', {
   maxZoom: 18,
-  maxNativeZoom: 20
+  maxNativeZoom: 22
 }).addTo(map);
 
-
-var osmb = new OSMBuildings(map).load();
+var osmb = new OSMBuildings(map);
 osmb.style({
   color: '#deebf4',
   roofColor: '#828886',
   shadows: true
 });
+
+exports.map = map;
+exports.osmb = osmb;
