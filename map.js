f2a06a51095a98d00c394f5575ba9eaebcabd245
Tigran Grigoryan <dqunbp@gmail.com>
2018-05-01 11:21:15 +0300

    add layers control

--- map.js
+++ map.js
@@ -1,10 +1,18 @@
-var map = new L.Map('map');
-map.setView([55.87835875564509, 37.7050219952363], 16, false);
 
-new L.TileLayer('http://mt0.google.com/vt/lyrs=s&hl=en&x={x}&y={y}&z={z}&s=Ga', {
-  maxZoom: 18,
-  maxNativeZoom: 22
-}).addTo(map);
+var google = L.tileLayer('http://mt0.google.com/vt/lyrs=s&hl=en&x={x}&y={y}&z={z}&s=Ga', { id: 1 }),
+  osm = L.tileLayer('http://a.tile.openstreetmap.org/{z}/{x}/{y}.png', { id: 2 }),
+  map = new L.Map('map', {
+    center: [55.87835875564509, 37.7050219952363],
+    zoom: 16,
+    layers: [google]
+  });
+
+var baseLayers = {
+  "Google": google,
+  "OSM": osm
+}
+
+L.control.layers(baseLayers, undefined, { position: 'topright', collapsed: false }).addTo(map);
 
 var osmb = new OSMBuildings(map);
 osmb.style({
